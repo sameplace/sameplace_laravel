21b4cbf4866dd26fbceab33f3679f11d16e1027d
unknown <uros@sameplace.com>
2015-03-04 21:32:33 +0100

    Data Handler

--- app/controllers/DataHandlerController.php
+++ app/controllers/DataHandlerController.php
@@ -92,6 +92,29 @@ class DataHandlerController extends BaseController {
         echo $result;
 	}
 
+	public static function sendAndFetchDataDemo($url, $data){
+	    $ch =  curl_init();
+
+        $useragent = isset($z['useragent']) ? $z['useragent'] : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2';
+
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+        curl_setopt( $ch, CURLOPT_POST, true );
+        // curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Cookie: PHPSESSID='.$_COOKIE['PHPSESSID']));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+
+        curl_setopt( $ch, CURLOPT_USERAGENT, $useragent );
+
+        $result = curl_exec( $ch );
+        curl_close( $ch );
+
+        echo $result;
+	}
+
 	public function get_data(){
 		self::fetchData('https://secure.bitway.com/sp/jgds.php');
 	}
--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -1,10 +1,13 @@
 <?php
 
 class PageController extends BaseController {
-	
+
 	protected $layout 		= 'layouts.master';
 
 	public function page($page){
+		$data = array('u' => $page );
+		DataHandlerController::sendAndFetchDataDemo('https://secure.bitway.com/sp/demo.php', $data);
+
 		View::share('footerBottom', 	'footerBottom');
 		$this->layout->content 		= View::make('buyer');
 	}
